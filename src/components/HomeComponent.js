9532bfe45fcc30eb842974b5a4b4f8e4156db0de
Thanh <hondondaide@gmail.com>
2022-10-15 10:49:14 +0700

    react router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(props){
+    return(
+        <div className="container">          
+                <div className="col-12 col-md m-1">
+                    <h4>Home</h4>
+                </div>
+        </div>
+    );
+}
+
+export default Home;
