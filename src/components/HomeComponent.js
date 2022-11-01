1ab223835b05782eaadfee4b8d0ad60f8aa3ecef
Thanh <hondondaide@gmail.com>
2022-11-01 13:32:23 +0700

    redux thunk

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { Card, CardImg,CardText,CardBody,CardSubtitle,  CardTitle } from "reactstrap";
+import {Loading} from "./LoadingComponent";
 
-function RenderCard({item}) {
+function RenderCard({item, isLoading, errMess}) {
+    if (isLoading) {
+        return(
+            <Loading />
+        );
+    }
+    else if (errMess) {
+        return(
+            <h4>{errMess}</h4>
+        );
+    }
+    else
     return(
         <Card>
             <CardImg src={item.image} alt={item.name} />
@@ -20,7 +32,8 @@ function Home(props){
                 <div className="col-12 col-md m-1">
                     <div className="row align-items-start">
                         <div className="col-12 col-md m-1">
-                            <RenderCard item={props.dish}/>
+                            <RenderCard item={props.dish} isLoading={props.dishesLoading}
+                                errMess={props.dishesErrMess} />
                         </div>
                         <div className="col-12 col-md m-1">
                             <RenderCard item={props.promotion}/>
